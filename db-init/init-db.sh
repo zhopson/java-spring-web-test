ffa64ad6552d2557e6b0a94512665091c441b318
andreechka <andreechka@localhost>
2020-12-20 13:27:24 +0900

    init db - adding create tables

--- db-init/init-db.sh
+++ db-init/init-db.sh
@@ -11,6 +11,27 @@ last_used timestamp not null,
 PRIMARY KEY (series)
 );
 
+CREATE TABLE app_role (
+role_id bigint not null,
+role_name varchar(30) not null,
+PRIMARY KEY (role_id)
+);
+
+CREATE TABLE app_user (
+user_id bigint not null,
+enabled boolean not null,
+encryted_password varchar(128) not null,
+user_name varchar(36) not null,
+PRIMARY KEY (user_id)
+);
+
+CREATE TABLE user_role (
+id bigint not null,
+role_id bigint not null,
+user_id bigint not null,
+PRIMARY KEY (id)
+);
+
 insert into App_User (USER_ID, USER_NAME, ENCRYTED_PASSWORD, ENABLED) values (2, 'dbuser1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', true);
 insert into App_User (USER_ID, USER_NAME, ENCRYTED_PASSWORD, ENABLED) values (1, 'dbadmin1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', true);
 insert into app_role (ROLE_ID, ROLE_NAME) values (1, 'ROLE_ADMIN');
