50334aecac19d88d4ebd1273a3978c1c26c8e7d9
MikhailovAN <MikhailovAN@WS-YAKGEO-1172.rosgeologia.corp>
2020-12-28 16:05:41 +0900

    init db changed

--- db-init/init-db.sh
+++ db-init/init-db.sh
@@ -32,8 +32,8 @@ user_id bigint not null,
 PRIMARY KEY (id)
 );
 
-insert into App_User (USER_ID, USER_NAME, ENCRYTED_PASSWORD, ENABLED) values (2, 'dbuser1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', true);
-insert into App_User (USER_ID, USER_NAME, ENCRYTED_PASSWORD, ENABLED) values (1, 'dbadmin1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', true);
+insert into App_User (USER_ID, USER_NAME, ENCRYTED_PASSWORD, ENABLED) values (2, 'dbuser1', '\$2a\$10\$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', true);
+insert into App_User (USER_ID, USER_NAME, ENCRYTED_PASSWORD, ENABLED) values (1, 'dbadmin1', '\$2a\$10\$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', true);
 insert into app_role (ROLE_ID, ROLE_NAME) values (1, 'ROLE_ADMIN');
 insert into app_role (ROLE_ID, ROLE_NAME) values (2, 'ROLE_USER');
 insert into user_role (ID, USER_ID, ROLE_ID) values (1, 1, 1);
